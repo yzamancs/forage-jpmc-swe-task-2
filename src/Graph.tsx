380f7571ace8bc47409ffdfde6c16e281ccc0ca7
uniqname <uniqname@umich.edu>
2023-07-17 14:38:57 -0700

    comments

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -51,12 +51,13 @@ class Graph extends Component<IProps, {}> {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
 
       // Add more Perspective configurations here.
+      // adding all elements for the graph
       elem.load(this.table);
       elem.setAttribute("view", "y_line");
-            elem.setAttribute("column-pivots", '["stock"]');
-            elem.setAttribute("row_pivots", '["timestamp"]');
-            elem.setAttribute("columns", '["top_ask_price"]');
-            elem.setAttribute("aggregates",
+      elem.setAttribute("column-pivots", '["stock"]');
+      elem.setAttribute("row_pivots", '["timestamp"]');
+      elem.setAttribute("columns", '["top_ask_price"]');
+      elem.setAttribute("aggregates",
                 '{"stock":"distinct_count", "top_ask_price":"avg", "top_bid_price":"avg", "timestamp":"distinct_count"}'
             );
     }
